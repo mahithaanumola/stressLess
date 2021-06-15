76716176742084be253f7efa4d77872c144de4e1
Mahitha Anumola <mahitha.anumola@gmail.com>
2021-06-15 16:01:27 -0400

    storyboard update

---
+++ stressLess/ViewController2.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController2.swift
+//  stressLess
+//
+//  Created by mahitha on 6/15/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
